6c704429e56a5015f1b3cef67b96b5b1f51f7d42
Kankuspasha <gaerejder@gmail.com>
2026-01-03 16:24:42 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,14 +1,10 @@
 import os
 import discord
-from discord import app_commands
 from discord.ext import commands
 
 # -------- INTENTS --------
 intents = discord.Intents.default()
 intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-
 
 bot = commands.Bot(
     command_prefix="!",
