b31072242004f1af9881b070bde8c160842629b7
Kankuspasha <gaerejder@gmail.com>
2026-01-03 15:43:13 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,7 +5,10 @@ from discord.ext import commands
 
 # -------- INTENTS --------
 intents = discord.Intents.default()
-intents.message_content = True  # galeri için gerekli
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 bot = commands.Bot(
     command_prefix="!",
